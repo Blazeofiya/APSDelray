94e8423209505b4902286a92566f1719a6805daf
Blazeofiya <130933252+Blazeofiya@users.noreply.github.com>
2025-01-10 16:54:01 -0500

    assets-js-script.js

--- assets-js-script.js
+++ assets-js-script.js
@@ -71,9 +71,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// Map Initialization
+// Initialize the map with custom tiles
 const map = L.map('map').setView([26.4615, -80.0728], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([26.4615, -80.0728]).addTo(map)
-    .bindPopup('Community Event: Beach Cleanup<br>Jan 15, 10 AM')
-    .openPopup();
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
+
+// Custom marker icon
+const customIcon = L.icon({
+  iconUrl: 'assets/images/custom-marker.png', // Replace with your custom marker image path
+  iconSize: [38, 38],
+  iconAnchor: [19, 38],
+  popupAnchor: [0, -38]
+});
+
+// Add marker with custom icon
+L.marker([26.4615, -80.0728], { icon: customIcon }).addTo(map)
+  .bindPopup('<b>Community Event:</b> Beach Cleanup<br><b>Date:</b> Jan 15, 10 AM')
+  .openPopup();
+
+// Custom zoom control position
+L.control.zoom({
+  position: 'bottomright' // Move zoom controls to the bottom-right corner
+}).addTo(map);
